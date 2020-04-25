31aa3091b6c56f63cdf23c846c74659353a91d38
Developer513 <tjwjs513@gmail.com>
2020-04-25 13:52:04 +0900

    4.25

--- assignment6/Account.java
+++ assignment6/Account.java
@@ -1,13 +1,13 @@
 package assignment6;
-
 import java.text.ParseException;    //구문 분석 중 오류가 예기치 않게 도달했음을 나타내는 클래스 
 //파싱은 어떤 페이지(문서, html 등)에서 내가 원하는 데이터를 특정 패턴이나 순서로 추출해 가공하는 것을 말한다.
 //먼저 파서는 컴파일의 일부로서 원시 프로그램의 명령문이나 온라인 명령문, HTML 문서 등에서 MArkup Tag 등을
-// 입력으로 받아들여서 구문을 해석할 수 있는 단위와 여러 부분으로 분할해주는 역할을 한다. 이러한 파서(parser) 역할을 하는 컴퓨터가 구문 트리(parse tree)로 재구성하는 구문 분석 과정을 뜻한다.
-import java.text.SimpleDateFormat;  //날짜 비교를 위한 날짜 클래스 
-import java.util.Date;      
-import java.util.Scanner;        //날짜와 시간에 관한 정보를 표현하는 클래스 
-import java.util.Arrays;
+// 입력으로 받아들여서 구문을 해석할 수 있는 단위와 여러 부분으로 분할해주는 역할을 한다. 이러한 파서(parser) 
+//역할을 하는 컴퓨터가 구문 트리(parse tree)로 재구성하는 구문 분석 과정을 뜻한다.
+import java.text.SimpleDateFormat;//현재날짜를 구하기 위한 클래스 
+import java.util.List;//동적배열 클래스 
+import java.util.*;
+
 
 public class Account {
    private double bal; // The current balance
@@ -127,13 +127,12 @@ class CheckingAccount extends Account{
 }
 
 class Bank {
-   private Account[] list;
-   //private int count;
+   List<Account> accountList; // 동적배열 선언 
    private int index = 0;
    public Bank() {
-      this.list = new Account[1];
+      this.accountList = new LinkedList<Account>();
    }
-   public void creatAccount(){//기능 메소드 매개변수로 문자열을 넣으면 그것과 맞는 객체 호출 
+   public void creatAccount(){//기능 메소드  문자열을 넣으면 그것과 맞는 객체 생성 및 배열 할당 
       Scanner sc = new Scanner(System.in);
       System.out.println("Please enter the number of type what you want");
       System.out.println("1.Regular Account  2.Saving Account  3.Student Account(withdraw limit)  4.CheckingAccount");
@@ -141,59 +140,54 @@ class Bank {
       System.out.print("Set the account number: ");
       int acnum = sc.nextInt();
       if(select == 1){
-         Account[] newList = Arrays.copyOf(this.list, this.list.length +1);
-         newList[index] = new Account(acnum);
+         accountList.add(new Account(acnum));
          index++;
       }
       if(select == 2){
          System.out.println("You choose Saving Account you can earn 1.21% interest!");
-         
-         list[index] = new SavingAccount(acnum); //다운캐스팅 
+         accountList.add(new SavingAccount(acnum));
          index++;
-       //  this.count = this.count+1;
-         
       }
       if(select == 3){
          System.out.println("You choose Student Account set the withdraw limit year");
          System.out.println("Example: insert \"2020\" ");
          String withdrawlimit = sc.next();
-         list[index] = new StudentAccount(acnum,withdrawlimit); //다운캐스팅 
+         accountList.add(new StudentAccount(acnum,withdrawlimit));
          index++;
-       //  this.count = this.count+1;
          System.out.println("Account can't withdraw until "+withdrawlimit+", however you can earn 2.42% interest!");
       }
       if(select == 4){
          System.out.println("You choose Checking Account set the overdraft limit");
          System.out.println("Example: insert \"3\" ");
          double overdraftlimit = sc.nextDouble();
-         list[index] = new CheckingAccount(acnum,overdraftlimit); //다운캐스팅 
+         accountList.add(new CheckingAccount(acnum,overdraftlimit));
          index++;
-       //  this.count = this.count+1;      
       }
    }
-   public void deleteAccount(){
+   public void deleteAccount(){//입력받은 accnum 과 일치하는 객체를 찾아 해당객체 삭제 
       Scanner sc = new Scanner(System.in);
       System.out.println("※CAUTION※ you must withdraw all blance before deletion");
       System.out.print("Please enter the account number to delete: ");
       Integer delaccountnum = sc.nextInt();//기본형이 아니라 참조형으로 바꾸면 call by value 방식으로 비교하는 .equals 를 사용할수 있다. 
-      for(int i=0;i<list.length;i++){
-         if(delaccountnum.equals(list[i].getAccountNumber()))  {
-            list[i]=null;
-            //this.count = this.count-1;
+      for(int i=0;i<accountList.size();i++){
+         //Account a = accountList.get(i);
+         if(accountList.get(i).getAccountNumber() == delaccountnum){ accountList.remove(i);
+//         if(delaccountnum.equals(accountList[i].getAccountNumber()))  {
             System.out.println("Account deletion Successful!");
-         }
+       }
       }
    }
-   public void giveInterest(){
-      for(int i=0;i<list.length;i++){
-         if(list[i] instanceof SavingAccount || list[i] instanceof StudentAccount){
-            if(list[i] instanceof SavingAccount){
+   public void giveInterest(){// 생생된 계좌 중 이자를 지급해야하는 계좌에 대해 이자 지급
+      for(int i=0;i<accountList.size();i++){
+         //Account a = accountList.get(i);
+         if(accountList.get(i) instanceof SavingAccount || accountList.get(i) instanceof StudentAccount){
+            if(accountList.get(i) instanceof SavingAccount){
                System.out.println("Interest was paid.");
-               ((SavingAccount) list[i]).giveInterest();
+               ((SavingAccount) accountList.get(i)).giveInterest();//다운캐스팅
             }
-            else if(list[i] instanceof StudentAccount){
+            else if(accountList.get(i) instanceof StudentAccount){
                System.out.println("Interest was paid.");
-               ((StudentAccount) list[i]).giveInterest();
+               ((StudentAccount) accountList.get(i)).giveInterest();//다운캐스팅
             }
          }
       }
@@ -201,26 +195,32 @@ class Bank {
    public static void main(String[] args){
       Bank account = new Bank();
       //이자율 1.21 % , 학생 우대 이자율 2.42 %
-      account.creatAccount();//Account accnum = 0
-      account.creatAccount();//SavingAccount accnum = 1 
-      account.creatAccount();//CheckingAccount accnum = 2 overdraft limit = 2
-      account.creatAccount();//StudentAccount accnum = 3 withdraw until = 2020
-      account.list[0].deposit(100);//Account에 100 입금
-      account.list[1].deposit(100);//SavingAccount 에 100 입금 
-      account.list[2].deposit(100);//CheckingAccount 에 100 입금 
-      account.list[3].deposit(100);//CStudentAccount 에 100 입금 
-      for(int i=0; i<account.list.length;i++){
-         account.list[i].print();
+      //account.creatAccount();//Account accnum = 0
+      //account.creatAccount();//SavingAccount accnum = 1 
+      //account.creatAccount();//CheckingAccount accnum = 2 overdraft limit = 3
+      account.creatAccount();//StudentAccount accnum = 3 withdraw until = 2021
+      //account.accountList.get(0).deposit(100);//Account에 100 입금
+      account.accountList.get(0).deposit(100);//SavingAccount 에 100 입금 
+      //account.accountList.get(1).deposit(100);//CheckingAccount 에 100 입금 
+      //account.accountList.get(3).deposit(100);//CStudentAccount 에 100 입금 
+      for(int i=0; i<account.accountList.size();i++){
+         account.accountList.get(i).print();
       }
       account.giveInterest();// 지급이 되어야 하는 계좌에 이자지급 
-      for(int i=0; i<account.list.length;i++){
-         account.list[i].print();
+      for(int i=0; i<account.accountList.size();i++){
+         account.accountList.get(i).print();
       }
-      for(int i=0; i<account.list.length;i++){
-         account.list[i].withdraw(100);
+      //for(int i=0; i<account.accountList.size();i++){
+         account.accountList.get(0).withdraw(100);
+      //account.accountList.get(1).withdraw(103);//한도초과 인출
+     // }
+      for(int i=0; i<account.accountList.size();i++){
+         account.accountList.get(i).print();
       }
-      for(int i=0; i<account.list.length;i++){
-         account.list[i].print();
-      }
+      //account.deleteAccount();
+      //account.deleteAccount(); 
+      //for(int i=0; i<account.accountList.size();i++){
+      //   account.accountList.get(i).print();
+     // }
    }
 }
